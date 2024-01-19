87122b84b4c1b237bc5a986bffa2e5bab71e4f83
jadeleonard <lukabartolomeo9@gmail.com>
2024-01-18 18:56:36 -0800

    prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,14 @@
-const getHero = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/api/blo', {
-      cache: 'no-store',
-    });
-    return response.json();
-  } catch (error) {
-    console.log('error', error);
-    throw error; // Re-throw the error to propagate it
-  }
+import React from 'react';
+
+
+
+
+const App = () => {
+    return (
+        <div className={}>
+
+        </div>
+    );
 };
 
-export default async function Appp() {
-  try {
-    const { hero } = await getHero();
-    return (
-      <div>
-        {/* Render the hero data */}
-        {hero && (
-          <div>
-            <p>Name: {hero.heroTitle}</p>
-            <p>Power: {hero.heroContent}</p>
-            {/* ... other properties */}
-          </div>
-        )}
-      </div>
-    );
-  } catch (error) {
-    console.error('Error in Appp:', error);
-    return (
-      <div>
-        {/* Render an error message */}
-        <p>Error fetching hero data.</p>
-      </div>
-    );
-  }
-}
+export default App;
